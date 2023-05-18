d58615b1903ad043afadd328812c48cc6eecfd4b
Szymon <113582467+szymon02913@users.noreply.github.com>
2023-05-18 12:42:32 +0200

    Add files via upload

---
+++ python/zad2.py
@@ -0,0 +1,3 @@
+a=float(input)(("Podaj pierwsza liczbę"))
+b=float(input)(("Podaj drugą liczbę"))
+print("Suma: ",a+b)
---
+++ python/zad3.py
@@ -0,0 +1,5 @@
+a=float(input)(("Podaj pierwsza liczbę"))
+b=float(input)(("Podaj drugą liczbę"))
+x=input("podaj operacje")
+
+print(eval(a+x+b))
